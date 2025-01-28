9f4520b5ee2aae45b5f508a8c875e199e9b2b221
unknown <josue@tec-software.com>
2025-01-27 22:13:21 -0300

    ADD, student detail

--- src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
+++ src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: false,
-  
+
   templateUrl: './nav-menu.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NavMenuComponent {
+  constructor(private router: Router) {}
 
+  logout(): void {
+    localStorage.removeItem('token');
+    // navigacion /auth/login
+    this.router.navigate(['auth', 'login']);
+  }
 }
--- src/app/modules/dashboard/pages/students/students.module.ts
+++ src/app/modules/dashboard/pages/students/students.module.ts
@@ -14,8 +14,9 @@ import { MatTableModule } from '@angular/material/table';
 import { SharedModule } from '../../../../shared/shared.module';
 import { StudentDialogFormComponent } from './components/student-dialog-form/student-dialog-form.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { StudentDetailComponent } from './pages/student-detail/student-detail.component';
 @NgModule({
-  declarations: [StudentsComponent, StudentDialogFormComponent],
+  declarations: [StudentsComponent, StudentDialogFormComponent, StudentDetailComponent],
   imports: [
     CommonModule,
     StudentsRoutingModule,
